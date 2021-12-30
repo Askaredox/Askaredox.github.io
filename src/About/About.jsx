a13d87e08e88847beef74a9a4351823b73dfcf2a
Askaredox <andyecarvajal@gmail.com>
2021-12-29 19:28:14 -0600

    about page done

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './About.css';
 
-import { Grid, Col, Panel, Whisper, Tooltip, Row } from "rsuite";
+import { Grid, Col, Panel, Whisper, Tooltip, Row, Button } from "rsuite";
 import { Icon } from "@rsuite/icons";
 
 import { FaQuestion } from "react-icons/fa";
@@ -24,11 +24,42 @@ class About extends React.Component {
             <Col lgHidden mdHidden style={{width:'100%'}}>
               <br/>
             </Col>
-            <Col lg={11} md={9}>
+            <Col lg={11} md={9} style={{textAlign: 'justify', textJustify: 'inter-word'}}>
               <h2>Andrés Carvajal</h2>
-              <p>Student of Computer Science Engineering coursing the last year of college.</p>
+              <p>
+                Student of Computer Science Engineering coursing the last year of college, with 4 years of 
+                programming experience using technologies such as Python3, C#, C++, Java, ReactJS, Git, 
+                Flutter, SQL.
+              </p>
+              <p>
+                I will help you with your projects. Working about a year 
+                in <a href='https://www.fiverr.com/askarmorales'><b>Fiverr</b></a> as a freelancer. 
+              </p>
+              <p>
+                Feel free to contact me, I <i>love</i> challenging 
+                projects and I'll make any research to solve any problem or task you have. 
+              </p>
               <br/>
-              <Panel bordered>
+              <Button href='https://www.fiverr.com/askarmorales' color="green" appearance="primary">
+                 See more...
+              </Button>
+            </Col>
+          </Row>
+          <br/>
+          <Row>
+            <Col lg={13} md={15}>
+              <Panel bordered style={{width:'90%'}}>
+                <h3>Category:</h3>
+                <p>Engineering</p>
+                <br/>
+                <h3>Ability:</h3>
+                <Whisper placement="bottomStart" trigger="hover" speaker={this.tooltip}>
+                  <p>Until done</p>
+                </Whisper>
+              </Panel>
+            </Col>
+            <Col lg={11} md={9}>
+              <Panel bordered style={{width:'90%'}}>
                 <h3>Strengths:</h3>
                 <img src="https://img.shields.io/badge/_-Python-black?style=plastic&logo=Python" alt="python" />&nbsp;
                 <img src="https://img.shields.io/badge/_-ReactJS-black?style=plastic&logo=react" alt="ReactJs" />&nbsp;
@@ -44,46 +75,15 @@ class About extends React.Component {
               </Panel>
             </Col>
           </Row>
-          <br/>
-          <Row>
-            <Col lg={13} md={15}>
-              <img src='https://media.giphy.com/media/cl83qB3OpgHZToeA6h/giphy.gif'></img>
-            </Col>
-            <Col lg={11} md={9}>
-              trasdf
-            </Col>
-          </Row>
         </Grid>
-
-
       </div>
     );
   }
   tooltip = (
     <Tooltip>
-      Work until the work is done.
+      Work until the job is done.
     </Tooltip>
   );
 }
-/**
- * 
-        <FlexboxGrid>
-          <FlexboxGrid.Item as={Col} colspan={24} md={12}>
-            <Panel bordered>
-              <h5>Category:</h5>
-              <p>Engineering</p>
-              <br/>
-              <h5>Ability:</h5>
-              <Whisper placement="bottomStart" trigger="hover" speaker={this.tooltip}>
-                <p>Until done</p>
-              </Whisper>
-            </Panel>
-          </FlexboxGrid.Item>
-          <FlexboxGrid.Item as={Col} colspan={24} md={12}>
-            <Panel bordered>
-              asdfas
-            </Panel>
-          </FlexboxGrid.Item>
-        </FlexboxGrid>
- */
+
 export default About;
